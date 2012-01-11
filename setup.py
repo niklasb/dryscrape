5941fe10d7d1c589217c1cb72b7332ca76b6eaf5
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-01-12 00:42:27 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Command
+
+setup(name='pyscrape',
+      version='0.8',
+      description='a lightweight Javascript-aware, headless web scraping library for Python',
+      author='Niklas Baumstark',
+      author_email='niklas.baumstark@gmail.com',
+      license='MIT',
+      url='https://niklasb.github.com/pyscrape',
+      packages=['pyscrape', 'pyscrape.driver']
+      )
