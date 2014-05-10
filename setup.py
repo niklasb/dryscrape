8d712cb9db0a5dd31046f3d6563a9c09de926795
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-05-10 03:23:20 +0200

    bump to 0.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Command
 
 setup(name='dryscrape',
-      version='0.8',
+      version='0.9',
       description='a lightweight Javascript-aware, headless web scraping library for Python',
       author='Niklas Baumstark',
       author_email='niklas.baumstark@gmail.com',
