f9650b3970c580edf11b370ce85bba4704fe03b2
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-07-23 20:38:24 +0200

    fix URL in PKG-INFO

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='dryscrape',
       author='Niklas Baumstark',
       author_email='niklas.baumstark@gmail.com',
       license='MIT',
-      url='https://niklasb.github.com/dryscrape',
+      url='https://github.com/niklasb/dryscrape',
       packages=['dryscrape', 'dryscrape.driver'],
       install_requires=['webkit_server', 'lxml'],
       )
